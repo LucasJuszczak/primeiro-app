703545fca7a3a5156530552d0b2f26d440c303b3
LucasJuszczak <161529868+LucasJuszczak@users.noreply.github.com>
2025-03-19 20:42:59 -0300

    Create Project

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app/app.module';
+
+/*
+- app.module.ts : É o módulo principal do aplicativo
+- app.controller.ts : Define as rotas e lida com as requisições
+- app.service.ts : Contém a lógica do negócio, separado do controlador
+*/
+
+// Metódo que inicia o nosso projeto
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
