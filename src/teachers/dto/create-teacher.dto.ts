6ef134eede14d6f4c3b2dc779abc4790af702326
LucasJuszczak <pplucasfernando@gmail.com>
2025-03-31 13:32:16 -0300

    Update 17 - Teachers + Users Dto and Entity

---
+++ src/teachers/dto/create-teacher.dto.ts
@@ -0,0 +1,6 @@
+
+export class CreateTeacherDto{
+    readonly name: string
+    readonly registration: string
+    readonly email: string
+}
---
+++ src/teachers/dto/update-teacher.dto.ts
@@ -0,0 +1,6 @@
+
+export class UpdateTeacherDto{
+    readonly name?: string
+    readonly registration?: string
+    readonly email ?: string
+}
