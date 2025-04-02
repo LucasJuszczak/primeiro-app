620a52b3fc9b3f769c63b79e71815c1596a5cdc9
LucasJuszczak <pplucasfernando@gmail.com>
2025-04-02 01:33:09 -0300

    Update 20 - Users controller and service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,36 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+
+    @Get()
+    findAllUsers(@Query('limit') limit: string){
+        console.log(limit)
+        return this.usersService.findAll()
+    }
+
+    @Get(':id')
+    findOneUser(@Param('id') id: string){
+        return this.usersService.findOne(id)
+    }
+
+    @Post()
+    createUser(@Body() createUserDto: CreateUserDto){
+        return this.usersService.create(createUserDto)
+    }
+
+    @Patch(':id')
+    updateUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto){
+        return this.usersService.update(id, updateUserDto)
+    }
+
+    @Delete(':id')
+    removeUser(@Param('id') id: string){
+        return this.usersService.remove(id)
+    }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,68 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
+import { User } from "./entities/user.entity";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UpdateUserDto } from "./dto/update-user.dto";
 
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    private users: User[] = [
+        {
+            id: 1,
+            name: "Usuário 01",
+            email: "user.usuario01@email.com"
+        }
+    ]
+
+    findAll(){
+        return this.users
+    }
+
+    findOne(id: string){
+        const user = this.users.find(user => user.id === Number(id))
+
+        if(user) return user
+        
+        throw new HttpException("This user doesn't exist!", HttpStatus.NOT_FOUND)
+    }
+
+    create(createUserDto: CreateUserDto){
+        const newId = this.users.length + 1
+
+        const newUser = {
+            id: newId,
+            ...createUserDto
+        }
+
+        this.users.push(newUser)
+
+        return newUser
+    }
+
+    update(id: string, updateUserDto: UpdateUserDto){
+        const userIndex = this.users.findIndex(user => user.id === Number(id))
+
+        if(userIndex < 0)
+            throw new HttpException("This user doesn't exist!", HttpStatus.NOT_FOUND)
+
+        const userItem = this.users[userIndex]
+
+        this.users[userIndex] = {
+            ...userItem,
+            ...updateUserDto
+        }
+
+        return "Updated User!!"
+    }
+
+    remove(id: string){
+        const userIndex = this.users.findIndex(user => user.id === Number(id))
+
+        if(userIndex < 0)
+            throw new HttpException("This user doesn't exist!", HttpStatus.NOT_FOUND)
+
+        this.users.splice(userIndex, 1)
+
+        return "Deleted User!!"
+    }
+}
