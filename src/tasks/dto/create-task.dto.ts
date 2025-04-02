62a04ab6f3dc54f835bf4114211cfd830a190a8c
LucasJuszczak <161529868+LucasJuszczak@users.noreply.github.com>
2025-04-02 20:03:01 -0300

    Update 22 - Install Mapped-types
    npm install @nestjs/mapped-types

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -5,7 +5,17 @@ Objetivo: Validar dados, transformar.
 uma determinada camada aceita e trabalha
 */
 
+import { IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator"
+
 export class CreateTaskDto{
+    @IsString({message:"O nome precisa ser um texto!"})
+    @IsNotEmpty({message:"O nome não pode ser vazio!"})
+//  @MinLength(5, {message:"O nome precisa ter no mínimo 5 caracteres!"})
+    @MaxLength(30, {message:"O nome precisa ter no máximo 30 caracteres!"})
     readonly name: string
+
+    @IsString({message:"A descrição precisa ser um texto!"})
+    @IsNotEmpty({message:"A descrição não pode ser vazia!"})
+    @MaxLength(200, {message:"A descrição precisar ter no máximo 200 caracteres!"})
     readonly description: string
 }
