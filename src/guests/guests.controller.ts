77b99ffe9ca3b69930c6a961db9b7251bfa00d94
LucasJuszczak <pplucasfernando@gmail.com>
2025-04-02 01:04:40 -0300

    Update 18 - Guests controller and service

--- src/guests/guests.controller.ts
+++ src/guests/guests.controller.ts
@@ -1,5 +1,36 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from "@nestjs/common";
+import { GuestsService } from "./guests.service";
+import { CreateGuestDto } from "./dto/create-guest.dto";
+import { UpdateGuestDto } from "./dto/update-guest.dto";
 
 
 @Controller('guests')
-export class GuestsController {}
+export class GuestsController {
+    constructor(private readonly guestsService: GuestsService) {}
+
+    @Get()
+    findAllGuests(@Query('limit') limit: string){
+        console.log(limit)
+        return this.guestsService.findAll()
+    }
+
+    @Get(':id')
+    findOneGuest(@Param('id') id: string){
+        return this.guestsService.findOne(id)
+    }
+
+    @Post()
+    createGuest(@Body() createGuestDto: CreateGuestDto){
+        return this.guestsService.create(createGuestDto)
+    }
+
+    @Patch(':id')
+    updateGuest(@Param('id') id: string, @Body() updateGuestDto: UpdateGuestDto){
+        return this.guestsService.update(id, updateGuestDto)
+    }
+
+    @Delete(':id')
+    removeGuest(@Param('id') id: string){
+        return this.guestsService.remove(id)
+    }
+}
--- src/guests/guests.service.ts
+++ src/guests/guests.service.ts
@@ -1,5 +1,70 @@
-import { Injectable } from "@nestjs/common";
-
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Guest } from './entities/guest.entity';
+import { CreateGuestDto } from './dto/create-guest.dto';
+import { UpdateGuestDto } from './dto/update-guest.dto';
 
 @Injectable()
-export class GuestsService {}
+export class GuestsService {
+    //Lista em memória para teste!
+    private guests: Guest[] = [
+        {
+            id: 1,
+            name: "Denji",
+            email: "denji@email.com",
+            presence: false
+        }
+    ]
+
+    findAll(){
+        return this.guests
+    }
+
+    findOne(id: string){
+        const guest = this.guests.find(guest => guest.id === Number(id))
+
+        if(guest) return guest
+
+        throw new HttpException("This guest doesn't exist!", HttpStatus.NOT_FOUND)
+    }
+
+    create(createGuestDto: CreateGuestDto){
+        const newId = this.guests.length + 1
+
+        const newGuest = {
+            id: newId,
+            ...createGuestDto,
+            presence: false
+        }
+
+        this.guests.push(newGuest)
+
+        return newGuest
+    }
+
+    update(id: string, updateGuestDto: UpdateGuestDto){
+        const guestIndex = this.guests.findIndex(guest => guest.id === Number(id))
+
+        if(guestIndex < 0)
+            throw new HttpException("This guest doesn't exist!", HttpStatus.NOT_FOUND)
+
+        const guestItem = this.guests[guestIndex]
+
+        this.guests[guestIndex] = {
+            ...guestItem,
+            ...updateGuestDto
+        }
+
+        return "Updated Guest!"
+    }
+
+    remove(id: string){
+        const guestIndex = this.guests.findIndex(guest => guest.id === Number(id))
+
+        if(guestIndex < 0)
+            throw new HttpException("This guest doesn't exist!", HttpStatus.NOT_FOUND)
+
+        this.guests.splice(guestIndex, 1)
+
+        return "Deleted Guest!!"
+    }
+}
