0302cc66faa55a03d49d0b4c07443e5f106d6b9a
LucasJuszczak <161529868+LucasJuszczak@users.noreply.github.com>
2025-04-02 19:33:17 -0300

    Update 21 - Install classes
    class-validator
    class-transformer

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 /*
 - app.module.ts : É o módulo principal do aplicativo
@@ -10,6 +11,7 @@ import { AppModule } from './app/app.module';
 // Metódo que inicia o nosso projeto
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe) //Trabalhar com validação global
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
